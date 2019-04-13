098d4e97df282fc512d84125526397e1b160bccf
Angel Ulise Tabares Gonzalez <taho2509@gmail.com>
2019-04-13 16:13:09 -0300

    [ADD] - creating and extending interfaces necesaries to use spotify-web-api-node

--- backend/spotify-access.js
+++
@@ -1,8 +0,0 @@
-const SpotifyWebApi = require('spotify-web-api-node')
-
-const spotifyApi = new SpotifyWebApi({
-  clientId: process.env.CLIENT_ID,
-  clientSecret: process.env.CLIENT_SECRET
-})
-
-module.exports = spotifyApi
