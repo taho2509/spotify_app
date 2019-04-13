6e66025a3d0056812a2d653f67b45a7abd421ecf
Angel Ulise Tabares Gonzalez <taho2509@gmail.com>
2019-04-12 21:58:01 -0300

    [ADD] - remodeling search bar and centering results

--- client/src/components/SpotifyBrowser.js
+++ client/src/components/SpotifyBrowser.js
@@ -1,17 +1,21 @@
-import React from 'react';
+import React, { Fragment }  from 'react';
 import axios from 'axios';
 import Form from 'react-bootstrap/Form';
+import Navbar from 'react-bootstrap/Navbar';
 import Card from 'react-bootstrap/Card';
 import CardColumns from 'react-bootstrap/CardColumns';
+import Button from 'react-bootstrap/Button';
+import FormControl from 'react-bootstrap/FormControl';
+import Alert from 'react-bootstrap/Alert';
 
-function ListItem(props) {
+function Album(props) {
     let album = props.album;
     return <Card style={{ width: '18rem' }}>
     <Card.Img variant="top" src={album.image} />
     <Card.Body>
       <Card.Title>{album.name}</Card.Title>
       <Card.Text>
-        Authors: {album.artists.join(" & ")}
+        Artists: {album.artists.join(" & ")}
       </Card.Text>
       <Card.Link href={album.url}>Open in spotify</Card.Link>
     </Card.Body>
@@ -20,34 +24,28 @@ function ListItem(props) {
 
 function AlbumList(props) {
     const albums = props.albums;
-    const listItems = albums.map((album) =>
-        <ListItem album={album}/>
+    const items = albums.map((album) =>
+        <Album album={album}/>
     );
     return (
-        <CardColumns>{listItems}</CardColumns>
+        <CardColumns Style="margin: 0px 200px;">{items}</CardColumns>
     );
   }
 
 class SpotifyBrowser extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            value: '',
-            searchedTerm: '',
-            searchResult: [],
-            error: false,
-            errorMessage: "",
-        };
-    
-        this.handleChange = this.handleChange.bind(this);
-        this.handleSubmit = this.handleSubmit.bind(this);
-      }
+  state = {
+    value: '',
+    searchedTerm: '',
+    searchResult: [],
+    error: false,
+    errorMessage: "",
+  }
 
-      handleChange(event) {
+      handleChange = (event) => {
         this.setState({value: event.target.value});
       }
 
-      handleSubmit(event) {
+      handleSubmit = (event) => {
         event.preventDefault();
         const data = {
             search: this.state.value
@@ -84,18 +82,33 @@ class SpotifyBrowser extends React.Component {
 
     render() {
         return (
-            <Form onSubmit={this.handleSubmit}>
-                <Form.Group controlId="formSearchSpotifyAlbum">
-                    <Form.Control 
-                        type="text" 
-                        placeholder="Enter album name"  
-                        value={this.state.value} onChange={this.handleChange}
-                    />
-                </Form.Group>
-                {this.state.searchedTerm ? <p>Results for query '{this.state.searchedTerm}':</p> : ''}
-                <AlbumList albums={this.state.searchResult}/>
-            </Form>
-        );
+          <Fragment>
+          <Navbar bg="light" expand="lg">
+            <Navbar.Brand href="#home">Spotify Albums Browser</Navbar.Brand>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav">
+              <Form inline onSubmit={this.handleSubmit}>
+                <FormControl type="text" placeholder="Enter album name" value={this.state.value} onChange={this.handleChange} className="mr-sm-2" />
+                <Button type="submit" variant="outline-success">Search</Button>
+              </Form>
+            </Navbar.Collapse>
+          </Navbar>
+          {
+            this.state.searchedTerm ? 
+            <Alert variant="success">
+              {this.state.searchResult.length} Results for query '{this.state.searchedTerm}'
+            </Alert> 
+            : ''
+          }
+          {
+            this.state.error ? 
+            <Alert variant="error">
+              Error '{this.state.errorMessage}':
+            </Alert> 
+            : ''
+          }
+          <AlbumList albums={this.state.searchResult}/>
+        </Fragment>);
     }
 }
 
