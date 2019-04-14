2205726b6f845990cdea964c7f2ec1f9557819c3
Angel Ulise Tabares Gonzalez <taho2509@gmail.com>
2019-04-14 15:42:02 -0300

    [ADD] - using async/await for readability

--- backend/src/albums.ts
+++ backend/src/albums.ts
@@ -18,37 +18,37 @@ db.once('open', () => console.log('connected to the database'))
 // checks if connection with the database is successful
 db.on('error', console.error.bind(console, 'MongoDB connection error:'))
 
-router.get('/', function (req, res) {
+router.get('/', async (req, res) => {
   if(!connectionStringProvided) {
-    res.status(500).json({ message: "Internal error"})
+    return res.status(500).json({ message: "Internal error"})
   }
-  spotifyApi.clientCredentialsGrant()
-    .then(data => {
-      spotifyApi.setAccessToken(data.body['access_token'])
-      return spotifyApi.searchAlbums(req.query.search)
-    })
-    .then((data: SearchAlbumsResponse) => {
-      let noImage = 'https://upload.wikimedia.org/wikipedia/commons/thumb/a/ac/No_image_available.svg/1024px-No_image_available.svg.png'
+  try{
+    let data = await spotifyApi.clientCredentialsGrant();
 
-      let albums = data.body.albums.items.map((item) => {
-        let album = new Album()
-        album.id = item.id
-        album.name = item.name
-        album.url = item.uri
-        album.image = item.images[0].url ? item.images[0].url : noImage
-        album.releaseDate = item.release_date
-        album.numberOfTracks = item.total_tracks
-        album.artists = item.artists.map((artist) => artist.name)
-        return album.save()
-      })
-
-      return Promise.all(albums)
+    spotifyApi.setAccessToken(data.body['access_token']);
+    let albumsResponse = await spotifyApi.searchAlbums(req.query.search);
+  
+    let noImage = 'https://upload.wikimedia.org/wikipedia/commons/thumb/a/ac/No_image_available.svg/1024px-No_image_available.svg.png'
+  
+    let albums = albumsResponse.body.albums.items.map((item) => {
+      let album = new Album()
+      album.id = item.id
+      album.name = item.name
+      album.url = item.uri
+      album.image = item.images[0].url ? item.images[0].url : noImage
+      album.releaseDate = item.release_date
+      album.numberOfTracks = item.total_tracks
+      album.artists = item.artists.map((artist) => artist.name)
+      return album.save()
     })
-    .then((values: IAlbumModel[]) => res.status(200).json({ albums: values }))
-    .catch((err: Error) => {
-      console.log('An error while searching albums occurred: ', err)
-      res.status(500).json({ message: "An error while searching albums occurred"})
-    })
+  
+    let albumModels = await Promise.all(albums);
+    res.status(200).json({ albums: albumModels });
+  }
+  catch(err) {
+    console.log('An error while searching albums occurred: ', err)
+    res.status(500).json({ message: "An error while searching albums occurred"})
+  }
 })
 
 export default router
