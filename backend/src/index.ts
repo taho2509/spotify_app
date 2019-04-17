ef5ea59dabc0fb99d6a0c31110a24d8dbdd10f44
Angel Ulise Tabares Gonzalez <taho2509@gmail.com>
2019-04-17 14:53:25 -0300

    [FIX] - docker image for backend

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,13 +1,13 @@
-import albumsHandler from './albums'
 import express from 'express'
 import bodyParser from 'body-parser'
 import cors from 'cors'
+import albumsHandler from './albums'
 
 var app = express()
 
-app.use(cors())
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
+app.use(cors())
 
 app.use('/albums', albumsHandler)
 
